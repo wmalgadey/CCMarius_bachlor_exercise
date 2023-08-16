f165890675319489847bf1fa0ebdd72d99e294e8
Wolfgang Malgadey <wolfgang.malgadey@computacenter.com>
2023-08-16 12:55:01 +0000

    Aufgabe 7 gefixed, anstatt geändert

--- src/src/Matrix.java
+++ src/src/Matrix.java
@@ -103,17 +103,19 @@ public class Matrix {
             }
         }
 
-        Arrays.sort(arr);
+        // easy
+        // Arrays.sort(arr);
 
-        // for (int i = 0; i < arr.length - 1; i++) {
-        // for (int j = 0; j < arr.length - i - 1; j++) {
-        // if (arr[j] > arr[j + 1]) {
-        // int temp = arr[j];
-        // arr[j] = arr[j + 1];
-        // arr[j + 1] = temp;
-        // }
-        // }
-        // }
+        // complicated :D
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
 
         index = 0;
         for (int row = 0; row < rows; row++) {
